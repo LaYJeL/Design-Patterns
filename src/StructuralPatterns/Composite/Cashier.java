ad0cc315e01ca7f12f8ad797416c690c58bac993
Yevhenii <yevhenii.ryzhuk@gmail.com>
2018-03-05 23:10:11 +0200

    Added composite pattern

---
+++ src/StructuralPatterns/Composite/Cashier.java
@@ -0,0 +1,58 @@
+package StructuralPatterns.Composite;
+
+/**
+ * 
+ * @author Yevhenii R
+ * 
+ * @date February 21, 2018
+ *
+ */
+
+public class Cashier implements Employee {
+
+	/*
+	 * In this class,there are many methods which are not applicable to cashier
+	 * because it is a leaf node.
+	 */
+	private String name;
+	private double salary;
+
+	public Cashier(String name, double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public void add(Employee employee) {
+		// this is leaf node so this method is not applicable to this class.
+	}
+
+	@Override
+	public Employee getChild(int i) {
+		// this is leaf node so this method is not applicable to this class.
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public double getSalary() {
+		return salary;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("==========================");
+		System.out.println("Name = " + getName());
+		System.out.println("Salary = " + getSalary());
+		System.out.println("==========================");
+	}
+
+	@Override
+	public void remove(Employee employee) {
+		// this is leaf node so this method is not applicable to this class.
+	}
+}
---
+++ src/StructuralPatterns/Composite/Main.java
@@ -0,0 +1,23 @@
+package StructuralPatterns.Composite;
+
+/**
+ * 
+ * @author Yevhenii R
+ * 
+ * @date February 21, 2018
+ *
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		Employee emp1 = new Cashier("Sohan Kumar", 20000.0);
+		Employee emp2 = new Cashier("Mohan Kumar", 25000.0);
+		Employee emp3 = new Accountant("Seema Mahiwal", 30000.0);
+		Employee manager1 = new BankManager("Ashwani Rajput", 100000.0);
+
+		manager1.add(emp1);
+		manager1.add(emp2);
+		manager1.add(emp3);
+		manager1.print();
+	}
+}
